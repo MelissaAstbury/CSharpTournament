86c158765a5c34043e232dcde594a02fd09be0f2
Melissa Astbury <melissaastbury@hotmail.com>
2021-05-13 16:22:48 +0100

    created the required classes for the tournament and also added in descriptive comments using triple slash

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        public TeamModel TeamCompeting { get; set; }
+        /// <summary>
+        /// Represents one team in the matchup.
+        /// </summary>
+
+        public double Score { get; set; }
+        /// <summary>
+        /// Represents the score for the particular team.
+        /// </summary>
+
+        public MatchupModel ParentMatchup { get; set; }
+        /// <summary>
+        /// Represents the matchup that this team came from as the winner.
+        /// </summary>
+    }
+}
